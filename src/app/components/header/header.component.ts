98ee92b7af5425147f8e27a689486c6189056860
Katherine856 <kathyuska97@gmail.com>
2023-12-21 23:26:29 -0500

    Detallito

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -14,24 +13,6 @@ export class HeaderComponent {
   @Output() stringEnviado = new EventEmitter<string>(); //Se declara el envio del valor del input de busqueda
 
   constructor() {}
-
-  //Nos permite capturar el tamaño de la pantalla
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
-    this.checkWidth();
-  }
-
-  ngOnInit() {
-    this.checkWidth();
-  }
-
-  //Verificar el tamaño actual de la pantalla
-  private checkWidth() {
-    const screenWidth = window.innerWidth;
-
-    // Definimos el valor de la pantalla en reponsive
-    this.isResponsive = screenWidth <= 768;
-  }
 
   // Accede al valor del input utilizando el evento
   onInputChange(event: Event){
